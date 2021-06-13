ef68f86f7c8e4b1014ee2741e137606efddcfc95
winsonluk <winson.luk@gmail.com>
2021-06-12 22:23:42 -0400

    add buttons

--- updatedmarket.py
+++ updatedmarket.py
@@ -41,10 +41,10 @@ closing_p1 = article[1].text
 closing_p2 = article[2].text
 closing_p3 = article[3].text
 
-columns = ['Time', 'DJIA', 'S&P 500', 'Nasdaq', 'Subject', 'Paragraph 1', 'Paragraph 2', 'Paragraph 3']
-opening = {'DJIA' : opening_dji, 'S&P 500' : opening_inx, 'Nasdaq' : opening_ixic, 'Subject' : opening_subject, 'Paragraph 1' : opening_p1, 'Paragraph 2' : opening_p2, 'Paragraph 3' : opening_p3}
-midday = {'DJIA' : midday_dji, 'S&P 500' : midday_inx, 'Nasdaq' : midday_ixic, 'Subject' : midday_subject, 'Paragraph 1' : midday_p1, 'Paragraph 2' : midday_p2, 'Paragraph 3' : midday_p3}
-closing = {'DJIA' : closing_dji, 'S&P 500' : closing_inx, 'Nasdaq' : closing_ixic, 'Subject' : closing_subject, 'Paragraph 1' : closing_p1, 'Paragraph 2' : closing_p2, 'Paragraph 3' : closing_p3}
+columns = ['Time', 'DJIA', 'S&P 500', 'Nasdaq', 'Subject', 'First Comment', 'Second Comment', 'Third Comment']
+opening = {'DJIA' : opening_dji, 'S&P 500' : opening_inx, 'Nasdaq' : opening_ixic, 'Subject' : opening_subject, 'First Comment' : opening_p1, 'Second Comment' : opening_p2, 'Third Comment' : opening_p3}
+midday = {'DJIA' : midday_dji, 'S&P 500' : midday_inx, 'Nasdaq' : midday_ixic, 'Subject' : midday_subject, 'First Comment' : midday_p1, 'Second Comment' : midday_p2, 'Third Comment' : midday_p3}
+closing = {'DJIA' : closing_dji, 'S&P 500' : closing_inx, 'Nasdaq' : closing_ixic, 'Subject' : closing_subject, 'First Comment' : closing_p1, 'Second Comment' : closing_p2, 'Third Comment' : closing_p3}
 df = pd.DataFrame([opening, midday, closing], index=[opening_date, midday_date, closing_date])
 df.to_csv('market.csv')
 
@@ -52,7 +52,10 @@ application = Flask(__name__)
 
 @application.route('/')
 def home():
-    return make_response(render_template_string(df.to_html()))
+    refresh = '<button type="button">Refresh</button>'
+    download_data = '<button type="button">Download Data (.csv)</button>'
+    download_code = '<button type="button">Download Code (.zip)</button>'
+    return make_response(render_template_string('&nbsp;|&nbsp;'.join((refresh, download_data, download_code)) + '<hr>' + df.to_html()))
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
