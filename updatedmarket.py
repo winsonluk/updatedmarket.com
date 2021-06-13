e57ec7ad80062f505ff46c9f403798a13073bce3
winsonluk <winson.luk@gmail.com>
2021-06-12 22:00:23 -0400

    format csv

--- updatedmarket.py
+++ updatedmarket.py
@@ -7,8 +7,8 @@ soup = BeautifulSoup(response.content, 'html.parser')
 article = soup.find_all('p', class_='article__p')
 subjects = soup.find_all('h3')
 
-opening_date = subjects[4].text
-opening_markets = article[8].text.split('\n')
+opening_date = subjects[4].text.split(' — ')[-1] + ' - Opening Comment'
+opening_markets = [i.split(': ')[-1] for i in article[8].text.split('\n')]
 opening_dji = opening_markets[0]
 opening_inx = opening_markets[2]
 opening_ixic = opening_markets[4]
@@ -18,8 +18,8 @@ opening_p1 = article[9].text
 opening_p2 = article[10].text
 opening_p3 = article[11].text
 
-midday_date = subjects[2].text
-midday_markets = article[4].text.split('\n')
+midday_date = subjects[2].text.split(' — ')[-1] + ' - Midday Comment'
+midday_markets = [i.split(': ')[-1] for i in article[4].text.split('\n')]
 midday_dji = midday_markets[0]
 midday_inx = midday_markets[2]
 midday_ixic = midday_markets[4]
@@ -29,8 +29,8 @@ midday_p1 = article[5].text
 midday_p2 = article[6].text
 midday_p3 = article[7].text
 
-closing_date = subjects[0].text
-closing_markets = article[0].text.split('\n')
+closing_date = subjects[0].text.split(' — ')[-1] + ' - Closing Comment'
+closing_markets = [i.split(': ')[-1] for i in article[0].text.split('\n')]
 closing_dji = closing_markets[0]
 closing_inx = closing_markets[2]
 closing_ixic = closing_markets[4]
@@ -39,6 +39,12 @@ closing_subject = subjects[1].text
 closing_p1 = article[1].text
 closing_p2 = article[2].text
 closing_p3 = article[3].text
+
+columns = ['Time', 'DJIA', 'S&P 500', 'Nasdaq', 'Subject', 'Comment']
+opening = {'DJIA' : opening_dji, 'S&P 500' : opening_inx, 'Nasdaq' : opening_ixic, 'Subject' : opening_subject, 'Comment' : opening_p1 + '\n' + opening_p2 + '\n' + opening_p3}
+midday = {'DJIA' : midday_dji, 'S&P 500' : midday_inx, 'Nasdaq' : midday_ixic, 'Subject' : midday_subject, 'Comment' : midday_p1 + '\n' + midday_p2 + '\n' + midday_p3}
+closing = {'DJIA' : closing_dji, 'S&P 500' : closing_inx, 'Nasdaq' : closing_ixic, 'Subject' : closing_subject, 'Comment' : closing_p1 + '\n' + closing_p2 + '\n' + closing_p3}
+df = pd.DataFrame([opening, midday, closing], index=[opening_date, midday_date, closing_date])
 
 print(opening_date)
 print('---')
@@ -90,4 +96,4 @@ print(closing_p2)
 print('---')
 print(closing_p3)
 
-pd.read_csv('market.csv')
+df.to_csv('market.csv')
